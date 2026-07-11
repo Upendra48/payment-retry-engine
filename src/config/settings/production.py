cc0c44ae4b8c7fb4579dafc181dfe7965be30b96
Upendra Raj Joshi <upendraofficial10@gmail.com>
2026-07-11 20:01:16 +0545

    Prepare project for Render deployment

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -1,3 +1,27 @@
 from .base import *
 
 DEBUG = False
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+X_FRAME_OPTIONS = "DENY"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
